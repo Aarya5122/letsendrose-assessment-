218cbc3674aa07f03136aa68e4f2cca1e0d30248
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-01-23 00:37:51 +0530

    CORS activated

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const cookieParser = require("cookie-parser");
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
+app.use(cors());
 
 const userRoutes = require("./routes/user.routes")
 app.use("/api1", userRoutes)
