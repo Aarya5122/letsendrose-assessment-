f4253ae6ce6249f4f8a45859d0531c0b25ddd05d
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-01-23 00:19:06 +0530

    Atlas linked updated, Start command altered

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = () => {
   mongoose.set("strictQuery", false);
   mongoose
-    .connect("mongodb://localhost:27017/letsendroseusers", {
+    .connect( `${process.env.MONGO_URL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
